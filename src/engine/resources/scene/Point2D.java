006ec091a8d7ad4bca9a1864ecc410c5f23cba25
Ziggeh <ziggy@projectswg.com>
2014-09-11 10:11:51 +0200

    Optimised Point2D and Point3D

--- src/engine/resources/scene/Point2D.java
+++ src/engine/resources/scene/Point2D.java
@@ -4,25 +4,21 @@ public class Point2D {
 	
 	public float x, z;
 	
-	public Point2D(float x, float z) {
+	public Point2D() {
 		
+	}
+	
+	public Point2D(float x, float z) {
 		this.x = x;
 		this.z = z;
-		
 	}
 	
 	public Point2D clone() {
-		
 		return new Point2D(x, z);
-		
 	}
 	
-	public float getDistance(Point2D target) {
-		
-		return (float)Math.sqrt(
-			Math.pow(x - target.x, 2) + 
-			Math.pow(z - target.z, 2));
-		
+	public float getDistance2D(Point2D target) {
+		return Math.abs(target.x - x) + Math.abs(target.z - z);
 	}
 
 }
